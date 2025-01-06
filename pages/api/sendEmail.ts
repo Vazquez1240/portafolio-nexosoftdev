232e5a3e643a4c715e14726e72ec6fa61ca990d4
martinDTV <martin.delatorre@monterrey.gob.mx>
2025-01-06 13:26:20 -0600

    README

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -73,8 +73,6 @@ export default async function handler(
 
     return res.status(200).json({ message: "Correo enviado exitosamente" });
   } catch (error) {
-    console.error("Error al enviar correo:", error);
-
     return res.status(500).json({ error: "Error al enviar correo" });
   }
 }
